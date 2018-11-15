345edef00d0380534ba568167345d0c5ad295899
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-11-15 22:10:15 +0900

    スイッチアニメーション修正　踏まれるたときスイッチのアニメーションをする処理

--- Unity/DeathGodAndAGirlsMoment/Assets/Scripts/SwichController.cs
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/SwichController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwichController : MonoBehaviour {
+    
     SimpleAnimation m_simpleAnimation;
-
+    
     // Use this for initialization
     void Start () {
-  
+        m_simpleAnimation = GetComponent<SimpleAnimation>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == ("shinigami"))
+        {
+            m_simpleAnimation.Play("Change");
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("shinigami"))
+        {
+            m_simpleAnimation.CrossFade("ReverseChange", 1f);
+        }
+
+    }
+
 }
