ace362b648730097e3024c17dc3534d9ffc093c1
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-12-01 19:32:57 +0900

    闇の塊画像差し替え　作業用シーン作成

---
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/DoorAkuryouDisPlay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAkuryouDisPlay : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] m_gimmickAkuryou;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_gimmickAkuryou[0] == null && m_gimmickAkuryou[1] == null && m_gimmickAkuryou[2] == null)
+        {
+            Destroy(gameObject);
+            m_gimmickAkuryou[3].SetActive(true);
+            m_gimmickAkuryou[4].SetActive(true);
+            m_gimmickAkuryou[5].SetActive(true);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "shinigami")
+        {
+            m_gimmickAkuryou[0].SetActive(true);
+            m_gimmickAkuryou[1].SetActive(true);
+            m_gimmickAkuryou[2].SetActive(true);
+        }
+    }
+}
---
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] m_gimmickAkuryou;
+    [SerializeField]
+    GameObject m_door;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (m_gimmickAkuryou[0] == null && m_gimmickAkuryou[1] == null && m_gimmickAkuryou[2] == null)
+        {
+            Destroy(m_door);
+        }
+	}
+}
