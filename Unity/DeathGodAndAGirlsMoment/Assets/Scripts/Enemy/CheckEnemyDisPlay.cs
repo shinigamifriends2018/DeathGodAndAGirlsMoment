2601ce3afe5ad4110dc97df2cf565b41271afd4c
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-12-05 14:43:03 +0900

    ステージ2悪霊出現処理

---
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Enemy/CheckEnemyDisPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckEnemyDisPlay : MonoBehaviour {
+
+    [SerializeField]
+    GameObject m_disPlayEnemy;
+    [SerializeField]
+    GameObject[] m_desEnemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_desEnemy[0] == null && m_desEnemy[1] == null)
+        {
+            m_disPlayEnemy.SetActive(true);
+        }
+    }
+}
