c512af4f968f983269002228e0c379eaada03a04
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-12-08 21:36:14 +0900

    ボス　アイドルアニメーション作成

---
+++ Unity/DeathGodAndAGirlsMoment/Assets/Picturs/Boss/BossController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossController : MonoBehaviour {
+
+    [SerializeField]
+    int m_hitPoint = 0;
+    // Use this for initialization
+    void Start () {
+        m_hitPoint = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_hitPoint == 0)
+        {
+            SceneManager.LoadScene("Clear");
+        }
+	}
+}
