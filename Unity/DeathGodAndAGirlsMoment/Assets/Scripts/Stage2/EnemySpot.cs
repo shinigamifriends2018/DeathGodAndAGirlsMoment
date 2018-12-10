87165243015dcf14745186e2b533e205071d6880
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2018-12-10 11:30:58 +0900

    ボスのかまいたちアニメーション、突撃アニメーション作成

---
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Stage2/EnemySpot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpot : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] m_EneDis;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "shinigami" || collision.gameObject.tag == "syoujo")
+        {
+            m_EneDis[0].SetActive(true);
+            m_EneDis[1].SetActive(true);
+        }
+    }
+}
