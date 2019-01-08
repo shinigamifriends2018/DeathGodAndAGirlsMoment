2879051e23dfe161859c4d4eb7fee9ab2589a9aa
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2019-01-08 16:40:26 +0900

    檻　スイッチ修正　

--- Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Title/Stage2Transition.cs
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Title/Stage2Transition.cs
@@ -10,7 +10,6 @@ public class Stage2Transition : MonoBehaviour {
     // Use this for initialization
     void Start () {
         int score = PlayerPrefs.GetInt("m_acquisitions[1]", 0);
-        PlayerPrefs.SetInt("score", score);
         m_scoreText.text = "recovery" + "   " + score + "%";
     }
 	
