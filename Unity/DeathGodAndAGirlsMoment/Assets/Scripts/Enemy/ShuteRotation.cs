dd9d47f4f0bcef5104cc247c173d130a4c22d876
LAPTOP-H0D93040\human <shinigamifriends2018@gmail.com>
2019-01-23 17:50:19 +0900

    悪霊弾修正中

--- Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Enemy/ShuteRotation.cs
+++ Unity/DeathGodAndAGirlsMoment/Assets/Scripts/Enemy/ShuteRotation.cs
@@ -20,7 +20,7 @@ public class ShuteRotation : MonoBehaviour{
             return;
         }
         var vec = (m_syoujo.transform.position - m_akuryou.transform.position).normalized;
-        m_akuryou.transform.rotation = Quaternion.FromToRotation(Vector3.left, vec);
+        m_akuryou.transform.rotation = Quaternion.FromToRotation(Vector2.left, vec);
     }
 
 }
